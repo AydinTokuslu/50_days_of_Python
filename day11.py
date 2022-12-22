8c02ae8d8949f14b6c3bf08c8664b2b64508a575
aydin <aydintokuslu78@gmail.com>
2022-12-23 00:06:24 +0300

    23.12.22

--- day11.py
+++ day11.py
@@ -5,23 +5,25 @@
 # example, ‘love’ and ‘evol’ should return True.
 
 def equal_strings(str1,str2):
-    ortak_eleman = []
+    farkli_eleman = ""
+    global random_list
+    random_list = []
     if len(str1) == len(str2):
         print("iki kelimenin uzunluğu aynıdır")
         for i in range(len(str1)):
-            if str1[i] in str2:
-                print("ortak eleman vardır.")
-                ortak_eleman.append(str1[i])
-                return True
-            else:
-                print("ortak eleman bulunmamaktadır.")
-                return False
-        print("ortak_elemanlar : ", ortak_eleman)
-
+            if str1[i] not in str2:
+                farkli_eleman += str1[i]
+            elif str2[i] not in str1:
+                farkli_eleman += str2[i]
     else:
         print("iki kelimenin uzunluğu aynı değildir")
         return False
 
-
+    if len(farkli_eleman) == 0:
+        print("iki kelime aynı harflere sahiptir.")
+        return True
+    else:
+        print("iki kelime aynı harflere sahip değildir.")
+        return False
 
 print(equal_strings("love", "evol"))
